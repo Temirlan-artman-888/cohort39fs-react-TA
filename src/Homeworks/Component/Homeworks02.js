d3cf9bca0f1abfd89e238383e2b46c1c2d91b086
“Temirlan-artman-888” <temiramanas@gmail.com>
2024-06-28 11:56:11 +0300

    added new files

---
+++ src/Homeworks/Component/Homeworks02.js
@@ -0,0 +1,14 @@
+import Card from './Card';
+
+
+const Homeworks02 =() => {
+    return(
+    <div className='homeworks02-container'>
+       <Card/>
+       <Card/>
+       <Card/>
+    </div>
+    );
+};
+
+export default Homeworks02;
