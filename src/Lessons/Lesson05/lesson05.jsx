f0543ffe76b6cbe080e321623c378a6ce4f766d8
“Temirlan-artman-888” <temiramanas@gmail.com>
2024-07-05 18:56:29 +0300

    added HomeWork4

---
+++ src/Lessons/Lesson05/lesson05.jsx
@@ -0,0 +1,15 @@
+import "./styles.css"
+
+function Lesson04() {
+    const buttonNames = [
+        "Burger",
+        "Fries",
+        "Cola",
+        "Salad",
+        "Ketchup",
+        "Ice-cream",
+    ];
+    return <div>
+        <Button/>
+    </div>
+};
